19138a9b1e94d7f55bf6ed3185cf4f14e29d32e2
Zakanawaner <mariofelectronica@gmail.com>
2023-10-11 12:14:44 +0200

    commit for pythonanywhere

--- app.py
+++ app.py
@@ -4,45 +4,43 @@ import os, json, base64
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-
-
-@app.route('/')
-def getGeneral():
-    return jsonify({'message': "Tremendo cumbiote papi"})
+with open('secret.json', 'r') as secrets:
+    wdir = json.load(secrets)['workingDir']
+    app.config['path'] = './' + wdir + 'static/'
     
 
 @app.route('/podcast-text/<nb>')
 def getPodcastText(nb):
-    with open('./static/strings.json', 'r') as archivo:
+    with open(app.config['path'] + 'strings.json', 'r') as archivo:
         datos = json.load(archivo)
         return jsonify({'message': datos['podcast'+ nb]})
 
 
 @app.route('/episodes-text')
 def getEpisodesText():
-    with open('./static/strings.json', 'r') as archivo:
+    with open(app.config['path'] + 'strings.json', 'r') as archivo:
         datos = json.load(archivo)
         return jsonify({'message': datos['episodes']})
 
 
 @app.route('/tales-text')
 def getTalesText():
-    with open('./static/strings.json', 'r') as archivo:
+    with open(app.config['path'] + 'strings.json', 'r') as archivo:
         datos = json.load(archivo)
         return jsonify({'message': datos['tales']})
 
 
 @app.route('/pdf/<filename>', methods=['GET'])
 def download_document(filename):
-    filepath = './static/tales/' + filename
+    filepath = app.config['path'] + 'tales/' + filename
     return send_file(filepath, as_attachment=True)
 
 
 @app.route('/get-tales', methods=['GET'])
 def get_tales():
-    pdf_directory = './static/tales'
-    img_directory = './static/images/'
-    with open('./static/strings.json', 'r') as archivo:
+    pdf_directory = app.config['path'] + 'tales/'
+    img_directory = app.config['path'] + 'images/'
+    with open(app.config['path'] + 'strings.json', 'r') as archivo:
         datos = json.load(archivo)
     available_documents = []
     documentList = [f for f in os.listdir(pdf_directory) if f.endswith('.pdf')]
@@ -62,7 +60,7 @@ def get_tales():
 
 @app.route('/about-text/<nb>')
 def getAboutText(nb):
-    with open('./static/strings.json', 'r') as archivo:
+    with open(app.config['path'] + 'strings.json', 'r') as archivo:
         datos = json.load(archivo)
         return jsonify({'message': datos['about' + nb]})
 
